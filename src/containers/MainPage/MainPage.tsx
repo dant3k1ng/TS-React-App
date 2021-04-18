bc8c0e4056c96d5e200a480fee59c7427738bb91
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-04-18 12:10:40 +0200

    Add content wrapper

--- src/containers/MainPage/MainPage.tsx
+++ src/containers/MainPage/MainPage.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 import Header from "../../shared/components/Header/Header";
+import Sidebar from "../../shared/components/Sidebar/Sidebar";
 
 const MainPageWrapper = styled.div`
+`;
+
+const ContentWrapper = styled.div`
+  padding-top: 16px;
+  display: flex;
+  flex-direction: row;
 `;
 
 class MainPage extends React.Component 
@@ -12,6 +19,12 @@ class MainPage extends React.Component
       return (
         <MainPageWrapper>
             <Header/>
+            <ContentWrapper>
+              <Sidebar/>
+              <div>
+                content
+              </div>
+            </ContentWrapper>
         </MainPageWrapper>
       );
     }
