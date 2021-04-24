98eb57a271b870d5c6d0b1d7885222cec3cde25e
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-04-24 13:26:28 +0200

    Add new colors

--- src/shared/styleHelpers/Colors.ts
+++ src/shared/styleHelpers/Colors.ts
@@ -2,8 +2,13 @@ const Color = {
     blue: "#3664bf",
     darkBlue: "#062769",
 
+    darkWhite: "#ECECEC",
+    
     gray: "#909090",
     darkGray: "#363636",
+
+    // color name by hex
+    Fiord: "#4B5274",
 }
 
 export default Color;
