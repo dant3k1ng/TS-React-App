31bf727b7912d25943b543f2ce71d3b7ef9fcd4e
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-04-24 13:26:13 +0200

    Change component name from Publication to PublicationItem

--- src/components/Publications/Latest/PublicationItem.tsx
+++ src/components/Publications/Latest/PublicationItem.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import Color from "../../../shared/styleHelpers/Colors";
-import FontSize from "../../../shared/styleHelpers/FontSizes";
+import ContentFooter from "./Publication/ContentFooter";
 
 const PublicationContainer = styled.div`
     height: 80px;
@@ -22,32 +22,6 @@ const Content = styled.div`
     justify-content: space-between;
 `;
 
-const ContentFooter = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    height: 20px;
-`;
-
-const Date = styled.span`
-    color: ${Color.gray};
-    font-size: ${FontSize[90]};
-`;
-
-const AuthorImage = styled.img`
-    border-radius: 100%;
-    object-fit: cover;
-    width: 20px;
-    height: 20px;
-    margin: 0 12px;
-`;
-
-const AuthorName = styled.span`
-    font-size: ${FontSize[90]};
-    font-weight: 900;
-    color: ${Color.darkBlue};
-`;
-
 const Title = styled.span`
     overflow: hidden;
     text-overflow: ellipsis;
@@ -59,23 +33,16 @@ const Title = styled.span`
     line-height: 1.2rem;
 `;
 
-function Publication() {
-    const date = "7 Jan. 2020";
-    const authorName = "John Doe";
-
+function PublicationItem() {
     return (
         <PublicationContainer>
             <Image src="img/man-5710164_640.jpg" alt="item"/>
             <Content>
                 <Title>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam enim odio, ullamcorper vitae semper eget, aliquam eget est.</Title>
-                <ContentFooter>
-                    <Date>{date}</Date>
-                    <AuthorImage src="img/avatar.jpg" alt="author"/>
-                    <AuthorName>{authorName}</AuthorName>
-                </ContentFooter>
+                <ContentFooter/>
             </Content>
         </PublicationContainer>
     );
 }
 
-export default Publication;
+export default PublicationItem;
