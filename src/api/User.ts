7251c3f0b22a7acab1ff60fbb55af816e857ee26
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-05-06 21:18:09 +0200

    User api interfaces

--- src/api/User.ts
+++ src/api/User.ts
@@ -1,3 +1,24 @@
+export interface IUserAddress {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+}
+
+export interface IUserCompany {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+}
+export interface IUser {
+    id: number;
+    name: string;
+    username: string;
+    address: IUserAddress;
+    phone: string;
+    website: string;
+    company: IUserCompany;
+}
 export const getUser = () => {
     return fetch("https://jsonplaceholder.typicode.com/users/1")
 }
