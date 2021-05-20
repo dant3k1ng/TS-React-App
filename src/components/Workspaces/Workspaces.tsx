27c1fa209c370a9e73ee25edd228ede9eb172382
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-05-20 22:03:15 +0200

    Workspace carousel

--- src/components/Workspaces/Workspaces.tsx
+++ src/components/Workspaces/Workspaces.tsx
@@ -1,18 +1,39 @@
 import styled from "styled-components";
 import WorkspaceItem from "./elements/WorkspaceItem";
+import Carousel, { slidesToShowPlugin } from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
 
 const ItemsContainer = styled.div`
     display: flex;
     flex-direction: row;
 `;
 
+const CustomCarousel = styled(Carousel)`
+.BrainhubCarousel__track {
+        padding: 10px 0 !important;
+    }
+`;
+
 function Workspaces() {
 
     return (
         <ItemsContainer>
-            <WorkspaceItem/>
-            <WorkspaceItem/>
-            <WorkspaceItem/>
+            <CustomCarousel itemWidth={250} plugins={[
+                'infinite',
+                'fastSwipe',
+                {
+                    resolve: slidesToShowPlugin,
+                    options: {
+                        numberOfSlides: 10
+                    }
+                },
+            ]}>
+                <WorkspaceItem />
+                <WorkspaceItem />
+                <WorkspaceItem />
+                <WorkspaceItem />
+                <WorkspaceItem />
+            </CustomCarousel>
         </ItemsContainer>
     );
 }
--- src/components/Workspaces/elements/WorkspaceItem.tsx
+++ src/components/Workspaces/elements/WorkspaceItem.tsx
@@ -7,6 +7,7 @@ const ItemContainer = styled.div`
     box-shadow: 0px 2px 3px 0px #cccccc;
     height: 180px;
     width: 240px;
+    min-width: 240px;
     margin: 0 12px 0 0;
     background-color: white;
     border-radius: 5px;
