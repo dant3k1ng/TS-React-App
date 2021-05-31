7b48c961b513050c50ecf5492d3bcddd35e32178
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-05-31 19:19:03 +0200

    Create user entity

--- src/api/User.ts
+++ src/api/User.ts
@@ -1,26 +1,3 @@
 export const getUser = () => {
     return fetch("https://jsonplaceholder.typicode.com/users/1")
 }
-
-export interface IUserAddress {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-}
-
-export interface IUserCompany {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-}
-
-export interface IUser {
-    id: number;
-    name: string;
-    username: string;
-    address: IUserAddress;
-    phone: string;
-    website: string;
-    company: IUserCompany;
-}
