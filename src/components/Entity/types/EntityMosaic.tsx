5d69600c8939e648da91a57f00691d9661eeeaa7
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-06-04 12:36:18 +0200

    Create EntityMosaic.tsx

---
+++ src/components/Entity/types/EntityMosaic.tsx
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import Color from "../../../shared/styleHelpers/Colors";
+import { IEntity } from "../interfaces/IEntity";
+
+const EntityWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    box-shadow: 2px 5px 10px #ECECEC;
+    background-color: white;
+    border-radius: 4px;
+    padding: 8px;
+    width: 100%;
+    max-width: 320px;
+`;
+
+const ImageWrapper = styled.div`
+    width: 80px;
+    min-width: 80px;
+    height: 80px;
+`;
+
+const Image = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 5px;
+`;
+
+const ContentWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding-left: 8px;
+`;
+
+const Title = styled.span`
+    color: ${Color.blue};
+    font-weight: 900;
+`;
+
+const Description = styled.span`
+    color: ${Color.gray};
+`;
+
+interface IProps {
+    entity: IEntity;
+}
+
+function Entity(props: IProps) 
+{
+    return (
+        <EntityWrapper>
+            <ImageWrapper>
+                <Image src={props.entity.imageUrl} alt="image"/>
+            </ImageWrapper>
+            <ContentWrapper>
+                <Title>{props.entity.title}</Title>
+                <Description>{props.entity.description}</Description>
+            </ContentWrapper>
+        </EntityWrapper>
+    );
+}
+
+export default Entity;
