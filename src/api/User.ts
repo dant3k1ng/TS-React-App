b83341d35808a74bac286613cf207b626b772d65
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-06-06 18:36:04 +0200

    Api for gettings users

--- src/api/User.ts
+++ src/api/User.ts
@@ -1,3 +1,7 @@
-export const getUser = () => {
-    return fetch("https://jsonplaceholder.typicode.com/users/1")
+export const getUsers = () => {
+    return fetch("https://jsonplaceholder.typicode.com/users")
+}
+
+export const getUser = (id: number = 1) => {
+    return fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
 }
