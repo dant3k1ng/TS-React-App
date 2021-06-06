45294a46bccec7fcfa407f3fd590684822a45001
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-06-06 18:53:09 +0200

    Logged user photo on app bootstrap

--- src/containers/AppBootstrap.tsx
+++ src/containers/AppBootstrap.tsx
@@ -5,16 +5,32 @@ import { getUser } from "../api/User";
 import { setUser } from "../redux/actions/UserAction";
 import { getWorkspaces } from "../api/Workspace";
 import { setWorkspace } from "../redux/actions/WorkspaceAction";
+import { getPhoto } from "../api/Photo";
+import { setUserPhoto } from "../redux/actions/UserPhotoAction";
 
 function AppBootstrap() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    getUser()
-      .then(response => response.json())
-      .then(data => {
-        dispatch(setUser(data));
-      });
+    async function getUserData()
+    {
+      const user = await getUser()
+        .then(response => response.json())
+        .then(data => {
+          dispatch(setUser(data));
+          return data;
+        });
+
+      if("id" in user) {
+        getPhoto(user.id)
+          .then(response => response.json())
+          .then(data => {
+            dispatch(setUserPhoto(data));
+          });
+      }
+    }
+
+    getUserData();
   });
 
   useEffect(() => {
